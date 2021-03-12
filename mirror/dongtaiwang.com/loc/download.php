91381a51f4d35c57801920ceaae251f5d07d58dc
dit-inc-us <contact@dit-inc.us>
2021-03-12 00:00:13 +0000

    Deploy
    Former-commit-id: dc402d2a865cc092b70853566ae3fcd4a3947e47

---
+++ mirror/dongtaiwang.com/loc/download.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>自由门7.95 (2021年03月03日)</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" href="/css/screen.css">
+	<script src="/js/respond.min.js"></script>
+</head>
+<body>
+    <div class="header">
+		<h1>镜像资料</h1>
+	</div>
+	<div class="container">
+		<div class="detail">
+	        <div class="article-box">
+		        自由门7.95 (2021年03月03日)
+<table border="1">
+            <tr>
+                <th width="375" scope="row"><div align="left">
+                    <b>自由门7.95专业版 </b>(2021年03月03日)<br>
+                    历史最久，使用人数最多。
+                </div></th>
+                <td width="297"><div align="left"><p>
+                    <a href="/mirror/dongtaiwang.com/loc/software/fg/795/fg795p.exe">下载exe</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="/mirror/dongtaiwang.com/loc/software/fg/795/fg795p.zip">下载zip</a>
+                </p></div></td>
+            </tr>
+
+            <tr>
+                <th width="375" scope="row"><div align="left">
+                    <b>自由门7.95限制版 </b>(2021年03月03日)<br>
+                    缺省只能上少数几个网站，进一步增强安全性。
+                </div></th>
+                <td width="297"><div align="left"><p>
+                    <a href="/mirror/dongtaiwang.com/loc/software/fg/795/fg795r.exe">下载exe</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="/mirror/dongtaiwang.com/loc/software/fg/795/fg795r.zip">下载zip</a>
+                </p></div></td>
+            </tr>
+			</table>
+	        </div>
+        </div>
+	</div>
+</body>
+</html>
