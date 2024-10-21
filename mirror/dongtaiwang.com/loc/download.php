16e0c3f24d76cb8b5eaef4645adbd75c4bcbf2e8
dit-inc-us <contact@dit-inc.us>
2024-10-21 19:00:16 +0000

    Deploy

---
+++ mirror/dongtaiwang.com/loc/download.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>自由门8.00 (2024年05月03日)</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" href="/css/screen.css">
+	<script src="/js/respond.min.js"></script>
+</head>
+<body>
+    <div class="header">
+		<h1>镜像资料</h1>
+	</div>
+	<div class="container">
+		<div class="detail">
+	        <div class="article-box">
+		        自由门8.00 (2024年05月03日)
+<table border="1">
+
+<tr>
+<th width="375" scope="row"><div align="left">
+<b>自由门8.00专业版 </b>(2024年05月03日)<br>
+历史最久，使用人数最多。
+</div></th>
+<td width="297"><div align="left"><p>
+<a href="/mirror/dongtaiwang.com/loc/software/fg/800/fg800p.exe">下载exe</a>&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="/mirror/dongtaiwang.com/loc/software/fg/800/fg800p.zip">下载zip</a>
+</p></div></td>
+</tr>
+<tr>
+<th width="375" scope="row"><div align="left">
+<b>自由门8.00限制版 </b>(2024年05月03日)<br>
+缺省只能上少数几个网站，进一步增强安全性。
+</div></th>
+<td width="297"><div align="left"><p>
+<a href="/mirror/dongtaiwang.com/loc/software/fg/800/fg800r.exe">下载exe</a>&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="/mirror/dongtaiwang.com/loc/software/fg/800/fg800r.zip">下载zip</a>
+</p></div></td>
+</tr>
+
+</table>
+
+
+<table border="1">
+
+<tr>
+<th width="375" scope="row"><div align="left"><b>自由门安卓VPN1.5</b>（2024年09月25日）</div></th>
+<td width="297"><div align="left"><p>
+<a href="/loc/dl/fgvpn.apk">下载apk</a>&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="/loc/dl/fgvpn_sha512.txt">SHA512签名</a>&nbsp;&nbsp;&nbsp;&nbsp;
+</p></div></td>
+</tr>
+
+<tr>
+<th colspan="2" align="left">
+应用名: <input type="text" size="7" id="apkName">
+包名: <input type="text" size="7" id="apkId"> <button type="button" onclick="genApk()">下载自定义版</button>
+</th>
+</tr>
+</table>
+	        </div>
+        </div>
+	</div>
+</body>
+</html>
