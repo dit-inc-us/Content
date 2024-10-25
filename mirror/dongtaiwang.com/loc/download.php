acde1cbeec2a3613d9b77b904671e8b70f3a1175
dit-inc-us <contact@dit-inc.us>
2024-10-25 01:00:17 +0000

    Deploy

---
+++ mirror/dongtaiwang.com/loc/download.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>自由门8.00 (2024年05月03日)</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" href="/css/screen.css">
+	<script src="/js/respond.min.js"></script>
+</head>
+<body>
+    <div class="header">
+		<h1>镜像资料</h1>
+	</div>
+	<div class="container">
+		<div class="detail">
+	        <div class="article-box">
+		        自由门8.00 (2024年05月03日)
+<table border="1">
+			<!-- fg download -->
+            <tr>
+                <th width="375" scope="row"><div align="left">
+                    <b>自由门8.00专业版 </b>(2024年05月03日)<br>
+                    历史最久，使用人数最多。
+                </div></th>
+                <td width="297"><div align="left"><p>
+                    <a href="/mirror/dongtaiwang.com/loc/software/fg/800/fg800p.exe">下载exe</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="/mirror/dongtaiwang.com/loc/software/fg/800/fg800p.zip">下载zip</a>
+                </p></div></td>
+            </tr>
+
+            <tr>
+                <th width="375" scope="row"><div align="left">
+                    <b>自由门8.00限制版 </b>(2024年05月03日)<br>
+                    缺省只能上少数几个网站，进一步增强安全性。
+                </div></th>
+                <td width="297"><div align="left"><p>
+                    <a href="/mirror/dongtaiwang.com/loc/software/fg/800/fg800r.exe">下载exe</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="/mirror/dongtaiwang.com/loc/software/fg/800/fg800r.zip">下载zip</a>
+                </p></div></td>
+            </tr>
+			<!-- fg download end -->
+		</table>
+
+
+<table border="1">
+            <!-- fgvpn download -->
+            <tr>
+                <th width="375" scope="row"><div align="left"><b>自由门安卓VPN1.5</b>（2024年09月25日）</div></th>
+                <td width="297"><div align="left"><p>
+                    <a href="/loc/dl/fgvpn.apk">下载apk</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="/loc/dl/fgvpn_sha512.txt">SHA512签名</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                </p></div></td>
+            </tr>
+            <!-- fgvpn download end -->
+            <tr>
+                <th colspan="2" align="left">
+                    应用名: <input type="text" size="7" id="apkName">
+                    包名: <input type="text" size="7" id="apkId"> <button type="button" onclick="genApk()">下载自定义版</button>
+                </th>
+            </tr>
+        </table>
+
+
+<table border="1"> 
+			<tr>
+				<th width="375" scope="row"><div align="left"><b>GProxy火狐工具条2.1.1版</b> （2016年1月26日）<br>
+				火狐浏览器Firefox和雷鸟电邮Thunderbird的附加软件，
+				让用户在火狐浏览器和雷鸟电邮中方便的切换代理服务器，支持世界通、自由门、无界、花园，支持正体、简体中文和英文界面。
+				</div></th>
+				<td width="297"><div align="left"><p>
+		      		<a href=/loc/software/gproxy_plus/gproxy_plus-2.1.1.zip>下载zip</a>&nbsp;&nbsp;&nbsp;&nbsp; 
+		      		<a href=/loc/software/gproxy_plus/gproxy_plus-2.1.1.xpi>下载xpi</a>&nbsp;&nbsp;&nbsp;&nbsp; 
+		      		<a href=/loc/software/gproxy_plus/gproxy_plus-2.1.1.xpi.asc>数字签名</a>&nbsp;&nbsp;&nbsp;&nbsp; 
+		      		<a href=/loc/software/gproxy_plus/gproxy_plus-2.1.1.xpi.md5.txt>数字指纹</a>&nbsp;&nbsp;&nbsp;&nbsp; 
+				</p></div></td>
+			</tr>
+			<tr>
+				<th width="375" scope="row"><div align="left"><b>网络嗅包工具:如意抓EasyCapture1.0</b> (2007年8月30日更新)</div></th>
+				<td width="297"><div align="left"><p>
+					<a href="/mirror/dongtaiwang.com/loc/software/other/EasyCapture-pkg.zip">下载</a>&nbsp;&nbsp;
+					<a href="/mirror/dongtaiwang.com/loc/software/other/EasyCapture-pkg.zip.asc">数字签名&nbsp;&nbsp;</a>
+				</p></div></td>
+			</tr>
+		</table>
+	        </div>
+        </div>
+	</div>
+</body>
+</html>
